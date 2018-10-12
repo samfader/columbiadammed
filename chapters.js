5ad3ec32fb07e36a22f034d4eaea5dfe439d9185
Sam Fader <sam.fader@mapbox.com>
2018-10-12 15:00:25 -0700

    change the flyto speed

--- chapters.js
+++ chapters.js
@@ -1,7 +1,7 @@
 var chapters = {
     'columbiaLake': {
         bearing: 27,
-        center: [-115.850000, 50.216667],
+        center: [-115.850000, 50.316667],
         zoom: 9.5,
         pitch: 20
     },
@@ -16,60 +16,60 @@ var chapters = {
         bearing: 90,
         center: [-118.193333, 51.049167],
         zoom: 15.3,
-        speed: 0.2
+        speed: 0.3
     },
     'keenleysideDam': {
         bearing: 45,
         center: [-117.771667, 49.338333],
         zoom: 15.3,
         pitch: 20,
-        speed: 0.2
+        speed: 0.3
     },
     'grandCouleeDam': {
         center: [-118.9775, 47.957222],
         zoom: 15.3,
-        speed: 0.2
+        speed: 0.3
     },
     'chiefJosephDam': {
         bearing: 90,
         center: [-119.638611, 47.995],
         zoom: 15.3,
         pitch: 40,
-        speed: 0.2
+        speed: 0.3
     },
     'wellsDam': {
         bearing: 90,
         center: [-119.864167, 47.947222],
         zoom: 15.3,
         pitch: 20,
-        speed: 0.2
+        speed: 0.3
     },
     'rockyReachDam': {
-        center: [-120.294722, 47.533056],
+        center: [-120.394722, 47.533056],
         bearing: 170,
         zoom: 15.2,
         pitch: 20,
-        speed: 0.2
+        speed: 0.3
     },
     'rockIslandDam': {
         bearing: 90,
         center: [-120.094444, 47.3425],
         zoom: 15,
-        speed: 0.2,
+        speed: 0.3,
         pitch: 40
     },
     'wanapumDam': {
         bearing: 90,
         center: [-119.970278, 46.877778],
         zoom: 15.3,
-        speed: 0.2
+        speed: 0.3
     },
     'priestRapidsDam': {
         bearing: 45,
         center: [-119.91, 46.644167],
         zoom: 15.3,
         pitch: 20,
-        speed: 0.2
+        speed: 0.3
     },
     'mcnaryDam': {
         duration: 6000,
@@ -77,26 +77,26 @@ var chapters = {
         bearing: 170,
         zoom: 15.2,
         pitch: 20,
-        speed: 0.2
+        speed: 0.3
     },
     'johnDayDam': {
         bearing: 90,
         center: [-120.693611, 45.715833],
         zoom: 15,
-        speed: 0.2,
+        speed: 0.3,
         pitch: 40
     },
     'theDallesDam': {
         bearing: 90,
         center: [-121.133889, 45.613889],
         zoom: 15.3,
-        speed: 0.2
+        speed: 0.3
     },
     'bonnevilleDam': {
         bearing: 45,
         center: [-121.940833, 45.645],
         zoom: 15.3,
         pitch: 20,
-        speed: 0.2
+        speed: 0.3
     },
 };
